7d5a600bf7aa6050c8d090b818eb00c797de4a81
WyroneBlue <wyroneblue@gmail.com>
2023-03-20 19:51:46 +0100

    css and cleanup

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 // Express Setup
 import express from 'express';
-
-import path, { join} from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Load env variables
 import dotenv from 'dotenv';
@@ -23,11 +22,10 @@ app.engine('hbs', engine({
     defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts/',
     partialsDir: __dirname + '/views/partials/',
-    // helpers: require('./utils/hbs-helpers')
+    helpers: import('./utils/hbs-helpers.js')
 }));
 app.set('view engine', 'hbs');
 app.set("views", "./views");
-// app.use('/public', express.static(join(__dirname, 'public')));
 app.use(express.static('public'))
 
 // Use Routes
