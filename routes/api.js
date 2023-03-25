b54fb589b1190b56bd138c740f1828c6be0b2426
WyroneBlue <wyroneblue@gmail.com>
2023-03-25 17:32:28 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,9 @@ const router = Router();
 import { getArtFavorites, getArt, searchArt, getArtById, getArtImages } from "../controllers/data.js";
 
 router.post('/favorites', getArtFavorites);
-router.get('/art', getArt);
-router.get('/art/search', searchArt);
-router.get('/art/details', getArtById);
 router.get('/art/details/images', getArtImages);
+router.get('/art/details', getArtById);
+router.get('/art/search', searchArt);
+router.get('/art', getArt);
 
 export default router;
