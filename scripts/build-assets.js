ddcc279eb0489609849d82b6280c076b3528ee66
WyroneBlue <wyroneblue@gmail.com>
2023-04-04 17:21:57 +0200

    fixed asset builder

--- scripts/build-assets.js
+++ scripts/build-assets.js
@@ -7,57 +7,18 @@ const PUBLIC_ASSETS = [
     'fonts',
     'icons',
     'images',
-    'manifest.json',
-    'sw.js',
-];
-
-const ROOT_ASSETS = [
-    'manifest.json',
-    'sw.js',
 ];
 
 (function() {
     PUBLIC_ASSETS.forEach((asset) => {
-        buildJS([`${RESOURCE_BASE}/${asset}/**/*.*` ], `${PUBLIC_BASE}/${asset}`);
+        buildJS([`${RESOURCE_BASE}/${asset}/*.*` ], `${PUBLIC_BASE}/${asset}`, '.*');
     });
 
-    return gulp.src([
-        `./resources/sw.js`,
-    ])
-    .pipe(minify({
-        noSource: true,
-        ext: {
-            min: '.js'
-        },
-    }))
-    .pipe(gulp.dest('./public'))
-
-    // ROOT_ASSETS.forEach((asset) => {
-    //     buildJS([`${RESOURCE_BASE}/${asset}`], PUBLIC_BASE, '.*');
-    // });
 })
 
-// (function () {
+(function() {
+    buildJS([`${RESOURCE_BASE}/sw.js` ], `${PUBLIC_BASE}`, '.js');
+    buildJS([`${RESOURCE_BASE}/manifest.json` ], `${PUBLIC_BASE}`, '.json');
+}());
 
-    // return gulp.src([
-    //     `./resources/sw.js`,
-    // ])
-    // .pipe(minify({
-    //     noSource: true,
-    //     ext: {
-    //         min: '.js'
-    //     },
-    // }))
-    // .pipe(gulp.dest('./public'))
-
-    // ROOT_ASSETS.forEach((asset) => {
-    //     buildJS([`${RESOURCE_BASE}/${asset}`], PUBLIC_BASE);
-    // });
-
-    // buildJS([`./resources/manifest.json`], './public');
-    // buildJS([`./resources/sw.js`], './public');
-
-    // buildJS([`${RESOURCE_BASE}/sw.js`], PUBLIC_BASE);
-    // buildJS([`${RESOURCE_BASE}/manifest.js`], PUBLIC_BASE);
-// })
 
